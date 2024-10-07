5af3ae05ab708b3358d1be8b8664ed7224d8011c
Marcos Floriano <marcos.floriano@sptech.school>
2024-10-07 10:51:58 -0300

    adicionei o campo disponibilidade
    na hora do merge ele não adicionou esse campo

--- src/main/java/com/example/back/entity/Agenda.java
+++ src/main/java/com/example/back/entity/Agenda.java
@@ -3,6 +3,7 @@ package com.example.back.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +19,11 @@ public class Agenda {
     @OneToOne
     @JoinColumn(name = "medico_id", nullable = false)
     private Medico medico;
+
+    @ElementCollection
+    @CollectionTable(name = "disponibilidade", joinColumns = @JoinColumn(name = "agenda_id"))
+    @Column(name = "disponibilidade")
+    private List<LocalDateTime> disponibilidade;
 
     @OneToMany(mappedBy = "agenda", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Agendamento> agendamentos = new ArrayList<>();
